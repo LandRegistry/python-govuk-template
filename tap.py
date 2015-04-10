d48446e59a67a58c6d44248843df2fe5b9d9dbb3
Michael Allen <michael@michaelallen.io>
2015-04-10 12:15:20 +0100

    copy the govuk_template files into locations we can use them from

--- tap.py
+++ tap.py
@@ -1,5 +1,10 @@
 import requests
 from os import path, makedirs
+from subprocess import call
+
+
+def thisDir():
+    return path.dirname(path.realpath(__file__))
 
 
 def latest_release():
@@ -21,20 +26,63 @@ def mustache_asset(release):
 
 
 def download(asset):
-    local_filename = './zip/' + asset['name']
+    local_filepath = './zip/' + asset['name']
     response = requests.get(asset['browser_download_url'], stream=True)
-    if not path.exists(path.dirname(local_filename)):
-        makedirs(path.dirname(local_filename))
-    with open(local_filename, 'wb') as f:
+    if not path.exists(path.dirname(local_filepath)):
+        makedirs(path.dirname(local_filepath))
+    with open(local_filepath, 'wb') as f:
         for chunk in response.iter_content(chunk_size=1024):
             if chunk:  # filter out keep-alive new chunks
                 f.write(chunk)
                 f.flush()
 
-    return local_filename
+    print('Downloaded ' + asset['name'] + ' to ' + local_filepath)
+
+    return local_filepath
 
 
 def download_mustache():
     release = latest_release()
     mustache = mustache_asset(release)
-    download(mustache)
+    path = download(mustache)
+    copy_assets(path)
+    copy_views(path, 'mustache')
+
+
+def unzip_path(tar_path):
+    unzipped_folder = path.basename(tar_path).replace('.tgz', '')
+    unzipped_path = thisDir() + "/zip/" + unzipped_folder
+    return unzipped_path
+
+
+def unzip(tar_path):
+    target_path = thisDir() + "/zip"
+    print('Unpacking ' + tar_path + ' to ' + target_path)
+    call(["tar", "-xf", tar_path, "-C", target_path])
+
+
+def copy_views(tar_path, name):
+    unzipped_path = unzip_path(tar_path)
+    source_view = unzipped_path + '/views/layouts/govuk_template.html'
+    target_view = thisDir() + '/views/' + name + '/govuk_template.mustache'
+
+    if not path.exists(unzipped_path):
+        unzip(tar_path)
+    if not path.exists(path.dirname(target_view)):
+        makedirs(path.dirname(target_view))
+
+    call(["cp", "-rf", source_view, target_view])
+    print('Copied ' + source_view + ' to ' + target_view)
+
+
+def copy_assets(tar_path):
+    unzipped_path = unzip_path(tar_path)
+    assets_path = thisDir()
+
+    if not path.exists(unzipped_path):
+        unzip(tar_path)
+    call(["cp", "-rf", unzipped_path + "/assets", assets_path])
+
+    print('Copied ' + unzipped_path + '/assets to ' + assets_path)
+
+download_mustache()
