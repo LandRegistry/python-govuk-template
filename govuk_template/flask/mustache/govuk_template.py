29f75217abf0c34f7b41186c454122f0db08611e
Michael Allen <michael@michaelallen.io>
2015-04-14 16:23:36 +0100

    switched to using pystache

--- govuk_template/flask/mustache/govuk_template.py
+++ govuk_template/flask/mustache/govuk_template.py
@@ -1,7 +1,14 @@
-from flask_stache import render_view
+from pystache.renderer import Renderer
+from os import path
+
+
+def thisDir():
+    return path.dirname(path.abspath(__file__))
 
 
 class GovukTemplate(object):
 
-    def render(self):
-        return render_view(self)
+    def render(self, **context):
+        renderer = Renderer(search_dirs=[thisDir()])
+        template = renderer.load_template('govuk_template')
+        return renderer.render(template, **context)
