27dcb6999db097e82f5a5c81f3824a24cd950946
Michael Allen <michael@michaelallen.io>
2015-05-01 13:01:14 +0100

    HOME isn't being set so we'll use JENKINS_HOME

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -1,11 +1,6 @@
 #! /usr/bin/env bash
 
-echo "$HOME"
-echo "$USER"
-echo "$JENKINS_HOME"
-echo "$WORKSPACE"
-
-env_dir="$HOME/.virtualenv/${JOB_NAME// /_}"
+env_dir="$JENKINS_HOME/virtualenv/${JOB_NAME// /_}"
 
 #create and activate a virtualenv
 virtualenv --clear $env_dir
